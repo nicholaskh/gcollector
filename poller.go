039dfea3f04d0974a980640f8b6b394250c80545
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-05-20 09:37:22 +0800

    new format of apache log

--- poller.go
+++ poller.go
@@ -82,7 +82,7 @@ func (this *Poller) filter(txt string) (tag string) {
 			if len(logPart) < 6 {
 				return ""
 			}
-			if logPart[5] == "500" {
+			if logPart[6] == "500" {
 				return tp
 			}
 		case LOG_TYPE_NGINX_404, LOG_TYPE_APACHE_404:
@@ -90,7 +90,7 @@ func (this *Poller) filter(txt string) (tag string) {
 			if len(logPart) < 6 {
 				return ""
 			}
-			if logPart[5] == "404" {
+			if logPart[6] == "404" {
 				return tp
 			}
 		case LOG_TYPE_PHP_ERROR:
